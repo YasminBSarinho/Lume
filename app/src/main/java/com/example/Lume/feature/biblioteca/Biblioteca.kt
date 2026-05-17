db26af821f7447255d4ffeaab1acf44c303051a5
YasminBSarinho <yasmiinsarinho@gmail.com>
2026-05-17 13:40:56 -0300

    exibição de status

--- app/src/main/java/com/example/Lume/feature/biblioteca/Biblioteca.kt
+++ app/src/main/java/com/example/Lume/feature/biblioteca/Biblioteca.kt
@@ -1,22 +1,42 @@
 package com.example.Lume.feature.biblioteca
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.Lume.feature.biblioteca.components.BibliotecaHeader
+import com.example.Lume.feature.biblioteca.components.BibliotecaStatus
+import com.example.Lume.feature.biblioteca.components.Status
 
 @Composable
 fun BibliotecaScreen() {
+    var statusEscolhido by remember {
+        mutableStateOf(Status.TODOS)
+    }
     Column(
         modifier = Modifier.padding(24.dp)
     ) {
         BibliotecaHeader(
             titulo = "Minha Biblioteca",
             subtitulo = "5 livros na sua coleção"
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        BibliotecaStatus(
+            statusEscolhido = statusEscolhido,
+            onStatusSelecionado = { novoStatus ->
+                statusEscolhido = novoStatus
+            }
         )
     }
 }
