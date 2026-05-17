6a90437aaba924d41516cdaf74f0542f334db893
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-05-17 16:59:22 -0300

    refactor: melhorando disposição dos componentes da tela de cadastro

--- app/src/main/java/com/example/Lume/feature/Cadastro/components/CadastroForms.kt
+++ app/src/main/java/com/example/Lume/feature/Cadastro/components/CadastroForms.kt
@@ -1,15 +1,15 @@
 package com.example.Lume.feature.Cadastro.components
 
-import android.R.attr.fontWeight
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,59 +20,82 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.Lume.ui.theme.LilasPrincipal
-import com.example.Lume.ui.theme.TextoPrincipal
-import com.example.Lume.ui.theme.TextoSecundario
-
 
 @Composable
-fun CadastroForms(){
-    
+fun CadastroForms() {
+
     var tituloLivro by remember { mutableStateOf("") }
     var autor by remember { mutableStateOf("") }
     var ano by remember { mutableStateOf("") }
     var genero by remember { mutableStateOf("") }
 
     Column(
+        modifier = Modifier.padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
-    )
-    {
+    ) {
+
         CampoCadastro(
-            titulo = "Titulo do Livro",
+            titulo = "Título do Livro",
             valor = tituloLivro,
-            onValueChange = { tituloLivro = it }
+            onValueChange = { tituloLivro = it },
+            modifier = Modifier.fillMaxWidth()
         )
-        CampoCadastro(
-            titulo = "Autor",
-            valor = autor,
-            onValueChange = { autor = it }
-        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+            CampoCadastro(
+                titulo = "Autor",
+                valor = autor,
+                onValueChange = { autor = it },
+                modifier = Modifier.weight(1f)
+            )
+
+            CampoCadastro(
+                titulo = "Ano",
+                valor = ano,
+                onValueChange = { ano = it },
+                modifier = Modifier.width(120.dp)
+            )
+        }
 
         CampoCadastro(
             titulo = "Gênero",
             valor = genero,
-            onValueChange = { genero = it }
-        )
-
-        CampoCadastro(
-            titulo = "Ano",
-            valor = ano,
-            onValueChange = { ano = it }
+            onValueChange = { genero = it },
+            modifier = Modifier.fillMaxWidth()
         )
 
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            ){
-            Botao(texto = "cadastrar")
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Botao(texto = "Cadastrar")
         }
     }
 }
 
+@Composable
+fun CampoCadastro(titulo: String, valor: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier
+) {
+
+    OutlinedTextField(
+        value = valor,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        label = {
+            Text(text = titulo)
+        },
+        singleLine = true
+    )
+}
 
 @Composable
 fun Botao(texto: String, modifier: Modifier = Modifier) {
+
     Button(
         onClick = {
             println("Livro cadastrado")
@@ -91,29 +114,8 @@ fun Botao(texto: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun CampoCadastro(titulo : String, valor: String, onValueChange: (String) -> Unit ){
-    Text(
-        text = titulo,
-        fontSize = 18.sp,
-        fontWeight = FontWeight.Bold,
-        color = TextoPrincipal
-    )
-    TextField(
-        value = valor,
-        onValueChange = onValueChange,
-        label = {
-            Text(
-            text = "Coloque o valor aqui",
-            color = TextoSecundario,
-
-        )},
-        modifier = Modifier.fillMaxWidth()
-        )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun CadastroFormsPreview(){
+fun CadastroFormsPreview() {
     CadastroForms()
 }
