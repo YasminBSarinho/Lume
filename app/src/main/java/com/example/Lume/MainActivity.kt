952685014bab37b065d8beb7b7977f8dffec04a5
Yasmin Sarinho <131291045+YasminBSarinho@users.noreply.github.com>
2026-06-08 23:29:27 -0300

    Revert "refactor: ajustando formulario de cadastro"
    This reverts commit 1adfa8ea73edd2f2f1300c629c4e40c4d655e1e5.

--- app/src/main/java/com/example/Lume/MainActivity.kt
+++ app/src/main/java/com/example/Lume/MainActivity.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.Lume.feature.Cadastro.CadastroScreen
 import com.example.Lume.feature.biblioteca.BibliotecaScreen
-import com.example.Lume.feature.sorteador.SorteadorScreen
 import com.example.Lume.ui.theme.LilasClaro
 import com.example.Lume.ui.theme.LilasPrincipal
 import com.example.Lume.ui.theme.LumeTheme
@@ -196,10 +195,10 @@ fun LivrosScreen() {
     BibliotecaScreen()
 }
 
-@Composable
-fun SortearScreen() {
-    SorteadorScreen()
-}
+//@Composable
+//fun SortearScreen() {
+//    SorteadorScreen()
+//}
 
 @Composable
 fun HomeScreen() {
